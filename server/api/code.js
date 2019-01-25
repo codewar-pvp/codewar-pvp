d4d32ebe6a5e3b14bc58aaee1403795fdec9d78d
Jason Schachter <jnschachter@gmail.com>
2019-01-25 17:57:38 -0500

    Added: route for code execution on backend

---
+++ server/api/code.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const {VM} = require('vm2')
+
+const vm = new VM({
+  timeout: 5000,
+  sandbox: {}
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const result = vm.run(`(
+            function () {
+                ${req.body.input}
+            }
+            )()`)
+
+    res.json({
+      output: result
+    })
+  } catch (err) {
+    if (err.message === 'Script execution timed out.') {
+      err.status = 400
+    }
+    next(err)
+  }
+})
+
+module.exports = router
