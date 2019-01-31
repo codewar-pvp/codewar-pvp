fff5a5543b51dbcb43f9436ff22b6e54262dfbd9
Stuart Jehn <stuartjehn@gmail.com>
2019-01-31 16:14:02 -0500

    signup form name field added with new thunk creator

--- server/auth/index.js
+++ server/auth/index.js
@@ -26,6 +26,7 @@ router.post('/login', async (req, res, next) => {
 })
 
 router.post('/signup', async (req, res, next) => {
+  console.log("signup body",req.body)
   try {
     const user = await User.create(req.body)
     req.login(user, err => (err ? next(err) : res.json(user)))
