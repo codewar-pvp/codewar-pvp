7500bb0148fde6e01344e0f719be34f825faaa52
shantang <shantang@aol.com>
2019-01-31 16:25:52 -0500

    addThreeQuestionwithTestSepcs

--- client/store/questionReducer.js
+++ client/store/questionReducer.js
@@ -42,6 +42,7 @@ export const fetchAllQuestions = () => {
 export default function(state = initialState, action) {
   switch (action.type) {
     case GOT_QUESTIONS:
+      console.log(action.questions)
       return {...state, questions: action.questions}
     case SELECT_QUESTION:
       return {...state, selectedQuestion: action.question}
--- script/seed.js
+++ script/seed.js
@@ -33,28 +33,29 @@ async function seed() {
   const questions = await Promise.all([
     Question.create({
       id: 1,
-      title: 'Two Sum Extreme',
+      title: 'Two Sum',
       description:
         ' Given an array of integers, find two numbers such that they add up to a specific target number.The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.You may assume that each input would have exactly one solution. Input: numbers={2, 7, 11, 15}, target=9 Output: index1=1, index2=2',
-      level: 'Hard',
+      level: 'Easy',
       rating: 3,
       author: 'Scott',
       category: 'ARRAY, CONTROL FLOW',
       testSpecs:
-        'function numericalCompare(a, b) {return a - b;}Test.assertSimilar(twoSum([1,2,3], 4).sort(numericalCompare), [0,2]);Test.assertSimilar(twoSum([1234,5678,9012], 14690).sort(numericalCompare), [1,2]);Test.assertSimilar(twoSum([2,2,3], 4).sort(numericalCompare), [0,1]);',
+        'describe(`${questionTitle} question`, () => {try {userOutput.forEach((item, idx) => {it(`The input for the question: ${JSON.stringify(input[idx])}, output expected to be a ${expectedOutputType}.`, () => {expect(item).to.be.a(expectedOutputType)})it(`Expected output: ${output[idx]}, instead got: ${item}`, () => {expect(item).to.eql(output[idx])})})} catch (error) {console.log("did not pass the test!")}})',
       funcHeader: 'function twoSum(numbers, target) { \n //code goes here \n }'
     }),
     Question.create({
       id: 2,
-      title: 'Binary Watch',
+      title: 'Insert Interval',
       description:
-        'A binary watch has 4 LEDs on the top which represent the hours (0-11)...',
-      level: 'Medium',
+        'Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary). You may assume that the intervals were initially sorted according to their start times.',
+      level: 'Hard',
       rating: 1,
       author: 'Jason',
       category: 'ARRAY, FUNDAMENTALS',
       testSpecs: 'Test specs will go here',
-      funcHeader: 'function twoSum(numbers, target) { \n //code goes here \n }'
+      funcHeader:
+        '/**\n* Definition for an interval.\n* function Interval(start, end) {\n*     this.start = start;\n*     this.end = end;\n* }\n*/\n/**\n* @param {Interval[]} intervals\n* @param {Interval} newInterval\n* @return {Interval[]}\n*/\n const insert = function(intervals, newInterval) { \n };'
     }),
     Question.create({
       id: 3,
@@ -80,14 +81,16 @@ async function seed() {
     }),
     Question.create({
       id: 5,
-      title: 'Happy Number',
-      description: 'The number should be happy',
-      level: 'Easy',
+      title: 'Random Pick with Blacklist',
+      description:
+        'Given a blacklist B containing unique integers from [0, N), write a function to return a uniform random integer from [0, N) which is NOT in B. Optimize it such that it minimizes the call to system’s Math.random(). Note: 1 <= N <= 1000000000 0 <= B.length < min(100000, N) [0, N) does NOT include N. See interval notation. \n Example 1: Input: ["Solution","pick","pick","pick"] [[1,[]],[],[],[]] Output: [null,0,0,0] \n  Example 2: Input: [["Solution","pick","pick","pick"][[2,[]],[],[],[]] Output: [null,1,1,1] \n  Example 3: Input: ["Solution","pick","pick","pick"][[3,[1]],[],[],[]] Output: [null,0,0,2] \n  Example 4: Input: ["Solution","pick","pick","pick"] [[4,[2]],[],[],[]] Output: [null,1,3,1] \n  Explanation of Input Syntax: The input is two lists: the subroutines called and their arguments. Solutions constructor has two arguments, N and the blacklist B. pick has no arguments. Arguments are always wrapped with a list, even if there are not any.',
+      level: 'Hard',
       rating: 2,
       author: 'Shan',
       category: 'ARRAY, ALGORITHMS',
       testSpecs: 'Test specs will go here',
-      funcHeader: 'function twoSum(numbers, target) { \n //code goes here \n }'
+      funcHeader:
+        '/** \n * @param {number} N \n * @param {number[]} blacklist*/ \n var Solution = function(N, blacklist) { \n }; \n /** \n * @return {number} \n */ \n Solution.prototype.pick = function() { \n   \n};\n /** \n* Your Solution object will be instantiated and called as such: \n* var obj = Object.create(Solution).createNew(N, blacklist) \n* var param_1 = obj.pick()\n*/'
     }),
     Question.create({
       id: 6,
@@ -120,6 +123,24 @@ async function seed() {
       output: '342',
       outputType: 'array',
       questionId: 6
+    }),
+    Test.create({
+      input: '[1, 2, 3], 4',
+      output: '[]',
+      outputType: 'array',
+      questionId: 1
+    }),
+    Test.create({
+      input: '[ 1234, 5678, 9012 ], 14690',
+      output: '[1,2]',
+      outputType: 'array',
+      questionId: 1
+    }),
+    Test.create({
+      input: '[2,2,3], 4',
+      output: '[0,1]',
+      outputType: 'array',
+      questionId: 1
     })
   ])
 
