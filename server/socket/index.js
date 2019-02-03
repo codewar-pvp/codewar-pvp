8c2cf16720a8bf228a15fff1f0fa6e7a0d1ec620
Scott DeMeo <sd3605@nyu.edu>
2019-02-03 11:10:18 -0500

    attached userid /session info to socket connection using the express-socket.io-session module

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,13 +1,24 @@
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
-
+    console.log(socket.handshake.session.passport || undefined)
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
 
     socket.on('challenge', user => {
       socket.broadcast.emit('challenge', user)
+    })
+
+    socket.on('login', function(userdata) {
+      socket.handshake.session.userdata = userdata
+      socket.handshake.session.save()
+    })
+    socket.on('logout', function(userdata) {
+      if (socket.handshake.session.userdata) {
+        delete socket.handshake.session.userdata
+        socket.handshake.session.save()
+      }
     })
   })
 }
