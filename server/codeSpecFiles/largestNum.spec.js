9fe49dc3a9f2b11681bd89cb387599b2d74d9e8c
Scott DeMeo <sd3605@nyu.edu>
2019-02-04 08:05:49 -0500

    finished largestNum.spec.js in /codeSpecFiles/ for backend and tested to ensure working properly

---
+++ server/codeSpecFiles/largestNum.spec.js
@@ -0,0 +1,49 @@
+const {VM} = require('vm2')
+const fs = require('fs')
+const {expect} = require('chai')
+const input = [[1, 2, 3], [7, 2, 3, 99, 3], [10, 23, 342, 87]]
+const output = [3, 99, 342]
+const userCode = fs.readFileSync(`./userCode-${process.env.sandboxId}.js`)
+
+describe(`largestNum question`, () => {
+  console.log('Log:')
+  input.forEach((item, idx) => {
+    const vm = new VM({
+      timeout: 5000,
+      sandbox: {
+        console: {
+          log(logStr) {
+            this.logHistory.push(logStr.toString())
+          },
+          logHistory: []
+        }
+      }
+    })
+    let result
+    try {
+      result = vm.run(`(function(){
+              return {
+                userCodeResult: (${userCode})(${JSON.stringify(item)}),
+                userConsoleHistory: console.logHistory
+              }
+            })()`)
+    } catch (error) {
+      console.log(error.stack)
+      throw new SyntaxError('code failure - stopping tests')
+    }
+    try {
+      const {userCodeResult, userConsoleHistory} = result
+      userConsoleHistory.forEach(line => console.log(line))
+      it(`The input for the question: (${JSON.stringify(
+        item
+      )}, output expected to be a number`, () => {
+        expect(userCodeResult).to.be.a('number')
+      })
+      it(`Expected output: ${output[idx]}, got: ${userCodeResult}`, () => {
+        expect(userCodeResult).to.eql(output[idx])
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  })
+})
