75117b5be90a567f557b8f67d8956e804e896e30
Stuart Jehn <stuartjehn@gmail.com>
2019-02-04 16:09:54 -0500

    commit for master merge

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,8 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    // console.log(`A socket connection to the server has been made: ${socket.id}`)
+    // console.log('userId', socket.handshake.session.passport || undefined)
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
