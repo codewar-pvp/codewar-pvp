b3b53ab8c89b17abd65bf076b9b8e605c6122de0
Scott DeMeo <sd3605@nyu.edu>
2019-02-05 12:22:23 -0500

    added online friend rooms to socket index.js file and dispatcher functions in front end socket logic

--- server/socket/index.js
+++ server/socket/index.js
@@ -4,22 +4,43 @@ module.exports = io => {
     console.log('userId', socket.handshake.session.passport || undefined)
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
+      console.log('socket.handshake.session =>', socket.handshake.session)
+
+      // emit leave event for all friend rooms:
+      const {user, friends} = socket.handshake.session.user
+      friends.forEach(friend => {
+        io.to(friend.name).emit('friendLeave', user.name)
+      })
+
+      // delete socket.handshake.session.user
+      // delete socket.handshake.session.passport
+      // socket.handshake.session.save()
+    })
+
+    socket.on('login', function(user) {
+      console.log('login event fired!')
+      socket.handshake.session.user = user
+      console.log('socket.handshake.session =>', socket.handshake.session)
+      socket.handshake.session.save()
+      const {name, friends} = socket.handshake.session.user
+      // join user to logged in room and emit join event for all friend rooms:
+      socket.join(`${name}`)
+      friends.forEach(friend => {
+        socket.join(friend.name)
+        io.to(friend.name).emit('friendJoin')
+      })
     })
 
     socket.on('challenge', user => {
       socket.broadcast.emit('challenge', user)
     })
 
-    socket.on('login', function(user) {
-      socket.handshake.session.user = user
-      socket.handshake.session.save()
-    })
-
     socket.on('logout', function(user) {
-      if (socket.handshake.session.user) {
-        delete socket.handshake.session.user
-        socket.handshake.session.save()
-      }
+      console.log('logged out')
+      // if (socket.handshake.session.user) {
+      //   delete socket.handshake.session.user
+      //   socket.handshake.session.save()
+      // }
     })
   })
 }
